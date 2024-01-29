310767aab9b827c2a35f847ce2d74ba7e25d797a
Jaber <jaberriyan357@gmail.com>
2024-01-29 17:00:23 +0600

    added

--- book_site/settings.py
+++ book_site/settings.py
@@ -17,11 +17,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://my-blog-1772.onrender.com','https://*.127.0.0.1']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -36,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
